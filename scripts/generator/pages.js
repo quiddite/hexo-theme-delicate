9a953a70ad15607747c65aae4fa1d13c2aef06b6
quiddite <quiddite4dino@gmail.com>
2025-09-11 00:39:31 +0800

    Update pages.js
    sadf

--- scripts/generator/pages.js
+++ scripts/generator/pages.js
@@ -32,7 +32,7 @@ hexo.extend.generator.register("_about", function (locals) {
     return {
       path: "about/index.html",
       data: locals.theme,
-      layout: "about",
+      layout: "archive",
     };
   }
 });
